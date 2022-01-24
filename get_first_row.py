1d2d77bb33d0dc80894475b99c45cc26ba312537
Your Name <ubaydullayev@samtuit.uz>
2022-01-24 21:43:35 +0500

    TASK get first row

--- get_first_row.py
+++ get_first_row.py
@@ -6,7 +6,13 @@ def get_first_row(data):
     Return:
         list: First row.
    """
+   first_row = []
+   row = data.split('\n')
+   first_row = row[1].split(',')
+   return first_row
    
-   return
+   
 
 # Read the csv file
+data = open('data.csv').read()
+print(get_first_row(data))
